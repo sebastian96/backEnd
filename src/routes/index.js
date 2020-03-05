8b95358a645bb573ce1f0e3898df55776c3039b2
Sebastian Miranda <arqcompu4@gmail.com>
2020-03-04 20:46:54 -0500

    push mater
    cambio en endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ router.get('/api:id', async (req, res) => {
     const questions = await pool.query(`SELECT rhq.id_restaurant, res.restaurant_name, rhq.id_question, que.question, que.id_type_question FROM restaurants_has_questions rhq LEFT JOIN questions que ON rhq.id_question = que.id_question LEFT JOIN restaurants res ON rhq.id_restaurant = ${id}`);
     
     const assamble = () => {
-        let res = [];
+        let res = []; 
         questions.forEach(element => {
             res.push({
                 id_question: element.id_question,
