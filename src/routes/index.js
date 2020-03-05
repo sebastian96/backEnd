d1f150966c5dcefefb76b4f53825327ab88f01ec
sebastian miranda <arqcompu4@gmail.com>
2020-03-04 22:28:27 -0500

    feature(index.js)
    Se actualiza consulta de preguntas y se agregan campos de opciones para cada pregunta.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,20 @@ const pool = require('../database');
 router.get('/api:id', async (req, res) => {
     const { id } = req.params;
 
-    const questions = await pool.query(`SELECT rhq.id_restaurant, res.restaurant_name, rhq.id_question, que.question, que.id_type_question FROM restaurants_has_questions rhq LEFT JOIN questions que ON rhq.id_question = que.id_question LEFT JOIN restaurants res ON rhq.id_restaurant = ${id}`);
+    const questions = await pool.query(`SELECT rhq.id_restaurant, res.restaurant_name, rhq.id_question, que.question, que.options, que.id_type_question FROM restaurants_has_questions rhq LEFT JOIN questions que ON rhq.id_question = que.id_question LEFT JOIN restaurants res ON rhq.id_restaurant = ${id}`);
     
     const assamble = () => {
         let res = [];
         questions.forEach(element => {
+            let options;
+            if(element.id_type_question !== 4) {
+               options = element.options.split('|');
+            }
             res.push({
                 id_question: element.id_question,
-                question: element.question
+                id_type_question: element.id_type_question,
+                question: element.question,
+                options
             })
         });
         return res;
